7eb62a846a824dff78fbe2f081a722ba1fcfce74
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-05-17 22:21:26 +0530

    Create Subarray with given sum.cpp

---
+++ Subarray with given sum.cpp
@@ -0,0 +1,58 @@
+/*Given an unsorted array A of size N that contains only positive integers, find a continuous sub-array that adds to a given number S and return the left and right index(1-based indexing) of that subarray.
+
+In case of multiple subarrays, return the subarray indexes which come first on moving from left to right.*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(vector<int>arr, int n, long long s)
+    {
+        // Your code here
+        long long initialSum = arr[0];
+        for (int i=0, j = 0;i<n;){
+            if(initialSum == s) return {i+1,j+1};
+            else if(initialSum >s && i<j) initialSum -= arr[i++];
+            else if(j<n-1) initialSum += arr[++j];
+            else break;
+        }
+        return {-1};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long long s;
+        cin>>n>>s;
+        vector<int>arr(n);
+        // int arr[n];
+        const int mx = 1e9;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        vector<int>res;
+        res = ob.subarraySum(arr, n, s);
+        
+        for(int i = 0;i<res.size();i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
