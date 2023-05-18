84c4240d12c7cc54d1791857c6119d3aee1ddb54
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-05-18 21:48:18 +0530

    Create Equilibrium Point.cpp

---
+++ Equilibrium Point.cpp
@@ -0,0 +1,65 @@
+/*Given an array A of n positive numbers. The task is to find the first Equilibrium Point in an array. 
+Equilibrium Point in an array is a position such that the sum of elements before it is equal to the sum of elements after it.
+*/
+
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+    
+        // Your code here
+        long long int sum = 0;
+        for(int i=0;i<n;i++)
+            sum += a[i];
+        long long int preSum = 0;
+        for(int i=0;i<n;i++){
+            long long int check = (sum-a[i]);
+            if(check==2*preSum)
+                return i+1;
+            preSum +=a[i];
+        }
+  return -1;
+        
+    }
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
