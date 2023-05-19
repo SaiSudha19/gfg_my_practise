f31f68b2a27ccb23b0d99d3f4bb45bb971b4ea6e
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-05-19 22:12:02 +0530

    Create Sum of Array Elements.cpp

---
+++ Sum of Array Elements.cpp
@@ -0,0 +1,44 @@
+//Given an integer array arr of size n, you need to sum the elements of arr
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+//Complete this function
+
+int sumElement(int arr[],int n)
+{
+    //Your code here
+    vector<int>v(arr,arr+n);
+    return accumulate(v.begin(),v.end(),0);
+}
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    
+	    cout<<sumElement(arr,n)<<endl;
+	    
+	}
+	return 0;
+}
+// } Driver Code Ends
