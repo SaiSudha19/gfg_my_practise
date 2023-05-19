89fa49faafc318c13e243f5be9b4ea98897e56b6
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-05-20 00:53:40 +0530

    Create Find difference between sum of diagonals.cpp

---
+++ Find difference between sum of diagonals.cpp
@@ -0,0 +1,40 @@
+//Given a matrix Grid[][] of size NxN. Calculate the absolute difference between the sums of its diagonals.
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int diagonalSumDifference(int N, vector<vector<int>> Grid) {
+        // code here
+        int sum1=0;
+        int sum2=0;
+        for(int i=0;i<N;i++){
+            sum1+=Grid[i][i];
+            sum2+=Grid[i][N-i-1];
+        }
+        return abs(sum1-sum2);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> Grid1(N, vector<int>(N));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) cin >> Grid1[i][j];
+        Solution ob;
+        cout << ob.diagonalSumDifference(N, Grid1) << "\n";
+    }
+}
+// } Driver Code Ends
