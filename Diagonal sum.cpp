89d958f4160f28831d7ed37348a2cc1f2f838a36
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-05-20 00:55:20 +0530

    Create Diagonal sum.cpp

---
+++ Diagonal sum.cpp
@@ -0,0 +1,45 @@
+//Given a square matrix of size n×n. Your task is to calculate the sum of its diagonals.
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    int DiagonalSum(vector<vector<int> >& matrix) {
+        // Code here
+        int sum1=0,sum2=0;
+        int N=matrix.size();
+        for(int i=0;i<N;i++){
+            sum1+=matrix[i][i];
+            sum2+=matrix[i][N-i-1];
+        }
+        return sum1+sum2;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>> matrix(n, vector<int>(n,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrix[i][j];
+			}
+		}
+		Solution ob;
+		int ans = ob.DiagonalSum(matrix);
+		cout << ans <<'\n';
+	}
+	return 0;
+}
+// } Driver Code Ends
