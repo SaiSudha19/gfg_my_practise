297afddbb0eed434be4101a9cd5cfa5008b1f60b
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-05-21 21:11:59 +0530

    Create Combinational Logic.cpp

---
+++ Combinational Logic.cpp
@@ -0,0 +1,33 @@
+/*Construct a 6 input gate which performs the following logical operation:
+ (not(A)).B + C.D +E.(not(F))
+where A, B, C, D, E and F are the inputs to the 6 input gate.*/
+
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int logicalOperation(int A,int B,int C,int D,int E,int F){
+        // code here
+        return ((~A)&B)|(C&D)|(E&(~F));
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int A, B, C, D, E, F;
+        cin >> A >> B >> C >> D >> E >> F;
+        Solution ob;
+        cout << ob.logicalOperation(A, B, C, D, E, F) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
