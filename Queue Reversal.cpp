64800359032ec3e965aad49ad0a801a7d73e6f0c
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-05-26 23:53:15 +0530

    Create Queue Reversal.cpp

---
+++ Queue Reversal.cpp
@@ -0,0 +1,54 @@
+//Given a Queue Q containing N elements. The task is to reverse the Queue. Your task is to complete the function rev(), that reverses the N elements of the queue.
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+queue<int> rev(queue<int> q);
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    queue<int> a=rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+// } Driver Code Ends
+
+
+//function Template for C++
+
+//Function to reverse the queue.
+queue<int> rev(queue<int> q)
+{
+    // add code here.
+     stack<int> s;
+    while(!q.empty()){
+        int element=q.front();
+        q.pop();
+        s.push(element);
+    }
+    
+    while(!s.empty()){
+        int element=s.top();
+        s.pop();
+        q.push(element);
+    }
+    return q;
+}
