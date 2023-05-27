62dc84a8800eaefd139f378061f78eff10b0b56b
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-05-27 23:31:00 +0530

    Create Modify Linked List-1.cpp

---
+++ Modify Linked List-1.cpp
@@ -0,0 +1,118 @@
+/*
+Given a singly linked list containing N nodes. Modify the Linked list as follows:
+
+1. Modify the value of the first half nodes such that 1st node's new value is equal to the value of the last node minus the first node's current value, 2nd node's new value is equal to the second last nodes value minus 2nd nodes current value, likewise for first half nodes.
+2. Replace the second half of nodes with the initial values of the first half nodes(values before modifying the nodes).
+3. If N is odd then the value of the middle node remains unchanged.
+*/
+
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+struct Node* modifyTheList(struct Node *head);
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void print(Node *head)
+{
+    Node *temp=head;
+	while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+Node is as follows:
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+class Solution{
+    public:
+    struct Node* modifyTheList(struct Node *head)
+    {
+        //add code here.
+        long long int N = 0;
+        if(head==NULL || head->next == NULL)
+            return head;
+            
+        stack<int> st;
+        struct Node *temp = head;
+        
+        while(temp!=NULL){
+            st.push(temp->data);
+            temp = temp->next;
+            N++;
+        }
+        
+        temp = head;
+        N /= 2;
+        
+        while(N--){
+            temp->data = st.top() - temp->data;
+            temp = temp->next;
+            st.pop();
+        }
+        
+        while(temp!=NULL){
+            temp->data = st.top();
+            temp = temp->next;
+            st.pop();
+        }
+        
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		struct Node *head = NULL,*temp=NULL;
+		while(n--){
+		    int a;
+            cin>>a;
+			Node *newNode = new Node(a);
+			if(head==NULL)
+                head=newNode;
+            else
+                temp->next=newNode;
+            temp=newNode;
+		}
+		Solution obj;
+		head = obj.modifyTheList(head);
+		print(head);
+		
+	}
+    return 0;
+}
+// } Driver Code Ends
