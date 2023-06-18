88a9862ea5fbe2574fce6b028f74771d2c38ff27
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-06-18 22:03:13 +0530

    Create Ticket Counter.cpp

---
+++ Ticket Counter.cpp
@@ -0,0 +1,59 @@
+//N people from 1 to N are standing in the queue at a movie ticket counter. It is a weird counter, as it distributes tickets to the first K people and then the last K people and again first K people and so on, once a person gets a ticket moves out of the queue. The task is to find the last person to get the ticket.
+
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int distributeTicket(int N, int K) {
+        // code here
+        int i=1,j=N,ans;
+        bool a=false;
+        while(i<=j)
+        {
+            if(!a)
+            {
+                i=i+K;
+                ans=j;
+            }
+            else 
+            {
+                j=j-K;
+                ans=i;
+            }
+            a=!a;
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        int K;
+        scanf("%d",&K);
+        
+        Solution obj;
+        int res = obj.distributeTicket(N, K);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
