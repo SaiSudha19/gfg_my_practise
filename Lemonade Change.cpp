d4458ef0e3e51d98ec787e28ebb2be7ec8adf1b3
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-06-22 21:35:41 +0530

    Create Lemonade Change.cpp

---
+++ Lemonade Change.cpp
@@ -0,0 +1,66 @@
+/*
+You are an owner of lemonade island, each lemonade costs $5. Customers are standing in a queue to buy from you and order one at a time (in the order specified by given array bills[]). Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill. You must provide the correct change to each customer so that the net transaction is that the customer pays $5.
+
+NOTE: At first, you do not have any bill to provide changes with. You can provide changes from the bills that you get from the previous customers.
+
+Given an integer array bills of size N where bills [ i ] is the bill the ith customer pays, return true if you can provide every customer with the correct change, or false otherwise.
+*/
+
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool lemonadeChange(int n, vector<int> &bills) {
+        // code here
+        int five=0,ten=0;
+        for(int i=0;i<n;i++){
+            if(bills[i]==20){
+                if(ten && five){
+                    ten--;
+                    five--;
+                }
+                else if(five>=3){
+                    five-=3;
+                }
+                else return false;
+            }
+            else if(bills[i]==10){
+                ten++;
+                if(five)five--;
+                else return false;
+            }
+            else five++;
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        vector<int> bills(N);
+        for (int i = 0; i < N; i++) cin >> bills[i];
+
+        Solution obj;
+        int ans = obj.lemonadeChange(N, bills);
+        if (ans)
+            cout << "True" << endl;
+        else
+            cout << "False" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
