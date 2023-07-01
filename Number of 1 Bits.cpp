90d844921db82c3e2a48817a3b3e54896a45072e
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-07-01 21:06:49 +0530

    Create Number of 1 Bits.cpp

---
+++ Number of 1 Bits.cpp
@@ -0,0 +1,40 @@
+//Given a positive integer N, print count of set bits in it. 
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        // Write Your Code here
+        int countone = 0;
+        
+        while(N > 0)
+        {
+            N = N & (N-1);
+            countone++;
+        }
+        return countone;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
