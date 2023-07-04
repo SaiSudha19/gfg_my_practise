9498cc60aed186cd9ebbd2163bf024b7d6b1da83
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-07-04 14:39:40 +0530

    Create Count the subarrays having product less than k.cpp

---
+++ Count the subarrays having product less than k.cpp
@@ -0,0 +1,49 @@
+//Given an array of positive numbers, the task is to find the number of possible contiguous subarrays having product less than a given number k.
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+         int cnt = 0;
+        int i = 0, j;
+        long long sum = 1;
+        for(j = 0; j < n; ++j) {
+            sum *= a[j];
+            while(i < j && sum >= k) {
+                sum /= a[i];
+                ++i;
+            }
+            if(sum < k) {
+                cnt += j-i+1;
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        long long int k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        cout << obj.countSubArrayProductLessThanK(arr, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
